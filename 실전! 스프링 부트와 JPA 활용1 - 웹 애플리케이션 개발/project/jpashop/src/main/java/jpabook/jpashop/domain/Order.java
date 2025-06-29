10351f203be20b62573d81e21718f10569b66422
taegyunB <taegyunb99@gmail.com>
2025-06-29 21:35:50 +0900

    엔티티 설계시 주의점

--- 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/project/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/project/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -8,6 +8,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static jakarta.persistence.FetchType.LAZY;
+
 @Entity
 @Table(name = "orders")
 @Getter
@@ -20,14 +22,14 @@ public class Order {
     private Long id;
 
     // 연관관계 주인
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")  // 매핑을 무엇으로 할건지 -> FK: member_id
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -35,4 +37,20 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;  // 주문상태 [ORDER, CANCEL]
+
+    //==연관관계 메서드==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
