71b0c55d5c4d8ae14e35c850b2775c734645b328
taegyunB <taegyunb99@gmail.com>
2025-07-13 15:43:42 +0900

    JPA에서 DTO로 바로 조회

--- 실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화/project/jpashop2/README.md
+++ 실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화/project/jpashop2/README.md
@@ -110,3 +110,26 @@
   - N+1 문제를 해결하는 핵심적인 방법
 
 ### 간단한 주문 조회 V4: JPA에서 DTO 바로 조회
+- repository -> order -> simplequery 폴더 생성
+  - OrderSimpleQueryRepositroy 파일 생성 후 쿼리를 따로 관리
+
+~~~java
+public List<OrderSimpleQueryDto> findOrderDtos() {
+    return em.createQuery(
+            "select new jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address)" +
+                    " from Order o" +
+                    " join o.member m" +
+                    " join o.delivery d", OrderSimpleQueryDto.class)
+            .getResultList();
+}
+~~~
+- 일반적인 SQL을 사용할 때 처럼 원하는 값을 선택해서 조회
+- new 명령어를 사용해서 JPQL의 결과를 DTO로 즉시 변환
+- SELECT 절에서 원하는 데이터를 직접 선택하므로 DB -> 애플리케이션 네트워크 용량 최적화(생각보다 마비)
+- 리포지토리 재사용성 떨어짐, API 스펙에 맞춘 코드가 리포지토리에 들어가는 단점
+
+**쿼리 방식 선택 권장 순서**
+1. 우선 엔티티를 DTO로 변환하는 방법을 선택한다.
+2. 필요하면 페치 조인으로 성능을 최적화 한다 -> 대부분의 성능 이슈가 해결된다.
+3. 그래도 안되면 DTO로 직접 조회하는 방법을 사용한다.
+4. 최후의 방법은 JPA가 제공하는 네이티브 SQL이나 스프링 JDBC Template을 사용해서 SQL을 직접 사용한다.
--- 실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화/project/jpashop2/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ 실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화/project/jpashop2/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -5,6 +5,8 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
 import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -26,6 +28,7 @@ import java.util.stream.Collectors;
 public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;  // 의존관계 주입
 
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
@@ -67,6 +70,16 @@ public class OrderSimpleApiController {
         return result;
     }
 
+    /**
+     * V4. JPA에서 DTO로 바로 조회
+     * - 쿼리 1번 호출
+     * - select 절에서 원하는 데이터만 선택해서 조회
+     */
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+       return orderSimpleQueryRepository.findOrderDtos();
+    }
+
     @Data
     static class SimpleOrderDto {
         private long orderId;
@@ -83,4 +96,5 @@ public class OrderSimpleApiController {
             address = order.getDelivery().getAddress();
         }
     }
+
 }
