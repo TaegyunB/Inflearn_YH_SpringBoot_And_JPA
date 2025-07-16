7ad895dd69d28a4863996aea5cf06f0796f90a94
taegyunB <taegyunb99@gmail.com>
2025-07-16 21:27:08 +0900

    V3: 엔티티를 조회해서 DTO로 변환(fetch join 사용)

--- 실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화/project/jpashop2/README.md
+++ 실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화/project/jpashop2/README.md
@@ -152,4 +152,28 @@ public List<OrderSimpleQueryDto> findOrderDtos() {
     => V4는 SELECT 절에서 필요한 컬럼만 선택해 DTO 생성자로 바로 전달함. 화면 의존성은 높아지지만, 불필요한 데이터 로딩을 줄여 성능에 유리할 수 있음
     
 ## 섹션 5. API 개발 고급 - 컬렉션 조회 최적화
-### 주문 조회 V1: 엔티티 직접 노출
+### 주문 조회 V3: 엔티티를 DTO로 변환 - 페치 조인 최적화
+~~~java
+public List<Order> findAllWithItem() {
+    return em.createQuery(
+            "select distinct o from Order o" + 
+                " join fetch o.member m" +
+                " join fetch o.delivery o" +
+                " join fetch o.orderItems oi" +
+                " join fetch oi.item i", Order.class)
+            .getResultList();
+}
+~~~
+- 페치 조인으로 SQL이 1번만 실행됨
+- distinct를 사용한 이유는 1대다 조인이 있으므로 데이터베이스 row가 증가함
+- 그 결과 같은 order 엔티티의 조회 수도 증가하게 됨
+- JPA의 distinct는 SQL에 distinct를 추가하고, 더해서 같은 엔티티가 조회되면, 애플리케이션에서 중복을 걸러줌
+- 위 예에서 order가 컬렉션 페치 조인 때문에 중복 조회 되는 것을 막아줌
+- 단점
+  - 페이징 불가능
+  - 컬렉션 페치 조인을 사용하면 페이징이 불가능함
+  - 하이버네이트는 경고 로그를 남기면서 모든 데이터를 DB에서 읽어오고, 메모리에서 페이징 해버림(매우 위험함)
+- 참고
+  - 컬렉션 페치 조인은 1개만 사용할 수 있음
+  - 컬렉션 둘 이상에 페치 조인을 사용하면 안됨
+  - 데이터가 부정합하게 조회될 수 있음
--- 실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화/project/jpashop2/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ 실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화/project/jpashop2/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,14 +1,21 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.jaxb.SpringDataJaxb;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -34,5 +41,69 @@ public class OrderApiController {
         }
 
         return all;
+    }
+
+    /**
+     * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
+     * - 트랜잭션 안에서 지연 로딩 필요
+     */
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))  // 변환하고자 하면 map을 사용하면 됨
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    /**
+     * V3. 엔티티를 조회해서 DTO로 변환(fetch join 사용 O)
+     * - 페이징 시에는 N 부분을 포기해야함(대신에 batch fetch size? 옵션 주면 N -> 1 쿼리로 변경 가능)
+     */
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Getter
+    static class OrderDto {
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Getter
+    static class OrderItemDto {
+
+        private String itemName;  // 상품명
+        private int orderPrice;  // 주문 가격
+        private int count;  // 주문 수량
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
     }
 }
